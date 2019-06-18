c54304ce5e3749b48ef2d479aace9ced67042169
rachelkathleen <rachel.hawa@gmail.com>
2019-06-18 17:02:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (list, partialName) {
+  let nameLength = partialName.length;
+  return list.filter(function (name) {
+    return name.slice(0, nameLength) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
